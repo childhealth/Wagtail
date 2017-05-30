aeea09ca4520df40f31000e55e118d034d37ef6b
childhealth <mike.kelly1@nhs.net>
2017-05-30 11:26:54 +0100

    Update models.py

--- models.py
+++ models.py
@@ -30,3 +30,53 @@ class DictionaryPage(Page):
         APIField('entryName'),
         APIField('entryDescription'),
     ]
+
+class ProviderPage(Page):
+    provID = models.CharField(max_length=50, unique=True)
+    provName = models.CharField(max_length=100, unique=True)
+    # provLogo - binary
+    provAddressLine1 = models.CharField(max_length=50)
+    provAddressLine2 = models.CharField(max_length=50)
+    provAddressLine3 = models.CharField(max_length=50)
+    provAddressLine4 = models.CharField(max_length=50)
+    provAddressLine5 = models.CharField(max_length=50)
+    provPostcode = models.CharField(max_length=10)
+    # provLat
+    # provLng
+    provTel = models.CharField(max_length=50)
+    provEmail = models.CharField(max_length=100)
+    provWeb = models.CharField(max_length=100)
+
+    content_panels = Page.content_panels + [
+        FieldPanel('provID', classname="full"),
+        FieldPanel('provName', classname="full"),
+        # provLogo
+        FieldPanel('provAddressLine1', classname="full"),
+        FieldPanel('provAddressLine1', classname="full"),
+        FieldPanel('provAddressLine2', classname="full"),
+        FieldPanel('provAddressLine3', classname="full"),
+        FieldPanel('provAddressLine4', classname="full"),
+        FieldPanel('provAddressLine5', classname="full"),
+        FieldPanel('provPostcode', classname="full"),
+        # provLat
+        # provLng
+        FieldPanel('provTel', classname="full"),
+        FieldPanel('provEmail', classname="full"),
+        FieldPanel('provWeb', classname="full"),
+        ]
+
+     api_fields = [
+        APIField('provID'),
+        APIField('provName'),
+        # provLogo
+        APIField('provAddressLine1'),
+                APIField('provAddressLine1'),
+                 APIField('provAddressLine1'),
+                 APIField('provAddressLine1'),
+                 APIField('provAddressLine1'),
+                 APIField('provAddressLine1'),
+                 APIField('provAddressLine1'),
+                 APIField('provAddressLine1'),
+                 APIField('provAddressLine1'),
+                 APIField('provAddressLine1'),
+    ]
